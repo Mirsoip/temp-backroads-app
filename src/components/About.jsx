ab4fe6400d3e97df22315e9e4ad544d1e9715819
Mirsoip Adilov <mirsoip.adilov@gmail.com>
2024-05-16 17:02:33 +0500

    second commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -25,7 +25,7 @@ const About = () => {
           <p>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur
             quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
-            unde dolor?
+            unde dolor?ngv
           </p>
           <a href="#about" className="btn">read more</a>
         </article>
